cfe4f41c75f0c5c3c6132f0df66db7f0c6500b68
Andrew Shek <andrew@tecky.io>
2023-05-27 12:10:08 +0800

    Appointment System Kick Start

---
+++ src/main/java/io/tecky/AppointmentSystem.java
@@ -0,0 +1,11 @@
+package io.tecky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentSystem {
+    public static void main(String[] args){
+        SpringApplication.run(AppointmentSystem.class,args);
+    }
+}
